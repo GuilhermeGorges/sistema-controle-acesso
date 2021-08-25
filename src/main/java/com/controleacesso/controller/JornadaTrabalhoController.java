f43545538ca69d1b577b1b8af8e151c3f1431849
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2021-08-25 19:59:10 -0300

    first commit

---
+++ src/main/java/com/controleacesso/controller/JornadaTrabalhoController.java
@@ -0,0 +1,34 @@
+package com.controleacesso.controller;
+
+import com.controleacesso.model.JornadaTrabalho;
+import com.controleacesso.service.JornadaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/jornada")
+public class JornadaTrabalhoController {
+    @Autowired
+    JornadaService jornadaService;
+
+    @PostMapping
+    public JornadaTrabalho createJornada(@RequestBody JornadaTrabalho jornadaTrabalho){
+        return jornadaService.saveJornada(jornadaTrabalho);
+    }
+
+    @GetMapping
+    public List<JornadaTrabalho> getJornadaTrabalhoList(){
+        return jornadaService.findAll();
+    }
+
+    @GetMapping("/{IdJornada}")
+    public ResponseEntity<JornadaTrabalho> getJornadaTrabalhoByID(@PathVariable("idJornada") Long idJornada) throws Exception {
+        return ResponseEntity.ok(jornadaService.getById(idJornada).orElseThrow(()->new NoSuchElementException("Not found!")));
+    }
+
+
+}
